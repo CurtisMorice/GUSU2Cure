de314af229ccaa6987b747240a79b12fd15551c1
sean zilka <zilka.sr@gmail.com>
2018-07-26 17:59:17 -0500

    added resource table to sql file

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 -- database name: gusu_project
 
--- create the user table
+-- create the users table
 CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
@@ -9,3 +9,24 @@ CREATE TABLE "users" (
     "email" VARCHAR (100) NOT NULL,
     "validated" BOOLEAN NOT NULL  
 );
+
+-- create the resources table
+CREATE TABLE "resources" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (200),
+    "url" VARCHAR (300),
+    "summary" VARCHAR (500),
+    "date_created" DATE
+);
+
+CREATE TABLE "resources" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (200),
+    "url" VARCHAR (300),
+    "summary" VARCHAR (500),
+    "date_created" DATE
+);
+
+
+-- insert into resources table
+INSERT INTO resources(name, url, summary, date_created) VALUES ('Organization Name', 'www.organization.com', 'This is the organization summary', '10/16/2018');
