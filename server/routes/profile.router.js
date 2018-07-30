741a0783c42593f23576717f14a0200e864fbfed
Law Yang <lawtouayang@gmail.com>
2018-07-30 13:41:06 -0500

    profile router rollback

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -7,18 +7,7 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-    const queryText = `SELECT username, bio, contact_info, email FROM profiles JOIN users ON user_id = users.id;`
-    pool.query(queryText)
-        .then((result) => {
-            console.log('profile page', result.rows);
-            res.send(result.rows)
-        })
-        .catch((error) => {
-            console.log('error getting user profiles', error);
-            res.sendStatus(500);
-        })
-});
+
 
 /**
  * POST route template
