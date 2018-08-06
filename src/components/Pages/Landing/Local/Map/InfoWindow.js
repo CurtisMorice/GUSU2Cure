5954259975927cc5e2004c181b5593b49d237791
sean zilka <zilka.sr@gmail.com>
2018-08-06 18:29:45 -0500

    got infowindows working

---
+++ src/components/Pages/Landing/Local/Map/InfoWindow.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+class InfoWindow extends React.Component {
+    
+    componentDidUpdate(prevProps){
+        if (this.props.map !== prevProps.map){
+            this.renderInfoWindow();
+        }
+
+        if(this.props.visible !== prevProps.visible || this.props.marker !== prevProps.marker){
+            this.props.visible ? this.openWindow() : this.closeWindow();
+        }
+
+        if(this.props.children !== prevProps.children){
+            this.updateContent();
+        }
+    }
+    
+    openWindow = () => {
+        this.infowindow
+            .open(this.props.map, this.props.marker);
+    }
+    closeWindow = () => {
+        this.infowindow.close();
+    }
+
+    renderInfoWindow(){
+        let {map, google, mapCenter} = this.props;
+
+        const iw = this.infowindow = new google.maps.InfoWindow({
+            content: ''
+        })
+
+        console.log('this.infowindow:', this.infowindow);
+        
+    }
+    
+    updateContent(){
+        const content = this.renderChildren();
+        this.infowindow.setContent(content)
+    }
+
+    renderChildren(){
+        const {children} = this.props;
+        console.log('children in iw:'. children);
+        
+        return ReactDOMServer.renderToString(children);
+    }
+
+    render() {
+      return null;
+    }
+  }
+
+export default InfoWindow;
