dd4fd94d78fa0acd2168a81e18532c255038eff6
Curtis Morice <curtismorice@gmail.com>
2018-08-10 19:39:18 -0500

    fixed errors in the Article Modal and New Article Table

--- src/components/Pages/Admin/Local/NewArticleTable.js
+++ src/components/Pages/Admin/Local/NewArticleTable.js
@@ -279,7 +279,7 @@ class NewArticleTable extends React.Component{
 
                           <TableCell component="th" scope="row">{newArticle.date_posted}</TableCell>
                           <TableCell component="th" scope="row">{newArticle.research_title}</TableCell> 
-                          <TableCell> <ArticleModal adminArticle={newArticle}/> </TableCell>
+                          <TableCell> <ArticleModal  adminArticle={newArticle}/> </TableCell>
                           {/* <TableCell component="th" scope="row">{newArticle.type}</TableCell>
                           <TableCell component="th" scope="row">{newArticle.phase}</TableCell> 
                           <TableCell component="th" scope="row">{newArticle.institution_name}</TableCell>
